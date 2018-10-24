346363745a33db29bd4c4b3d2c8858706f54e7df
fuwentao <847232901@qq.com>
2018-10-24 11:31:40 +0800

    reservoir sampling

--- Permutation.java
+++ Permutation.java
@@ -1,5 +1,6 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class Permutation {
 
@@ -7,13 +8,24 @@ public class Permutation {
 
 		RandomizedQueue<String> queue = new RandomizedQueue<String>();
 		int num = Integer.parseInt(args[0]);
-
+		int count = 0;
 		while (!StdIn.isEmpty()) {
+
 			String item = StdIn.readString();
-			queue.enqueue(item);
+			if (++count <= num) {
+
+				queue.enqueue(item);
+			} else {
+
+				if (StdRandom.uniform(1, count + 1) <= num) {
+					queue.dequeue();
+					queue.enqueue(item);
+				}
+			}
 		}
 
 		for (String s : queue) {
+
 			if(num == 0)
 				break;
 			StdOut.println(s);
